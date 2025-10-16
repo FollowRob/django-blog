00c84732d5cce47334088790109b07a6146b2ddf
Rob Smith <rsmith0393@gmail.com>
2025-10-16 15:11:23 +0100

    create about app

--- blog/views.py
+++ blog/views.py
@@ -6,24 +6,21 @@ from .models import Post
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
-    paginate_by = 6
+    paginate_by = 12
 
 
+def event_detail(request, event_id):
+    
+    queryset = Event.objects.all()
+    event = get_object_or_404(queryset, event_id=event_id)
 
+    return render(
+        request,
+        "events/event_detail.html",
+        {"event": event}
+    )
 
 def post_detail(request, slug):
-    """
-    Display an individual :model:`blog.Post`.
-
-    **Context**
-
-    ``post``
-        An instance of :model:`blog.Post`.
-
-    **Template:**
-
-    :template:`blog/post_detail.html`
-    """
 
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
